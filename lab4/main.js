e26457e45e7e81276e2fef960a9864e1de0ef315
leormix <82319032+leormix@users.noreply.github.com>
2026-04-01 16:52:23 +0300

    адед лоджік фор сьорчінг

--- lab4/main.js
+++ lab4/main.js
@@ -11,4 +11,32 @@ class BiDirectionalPriorityQueue {
             order: this.insertCounter++
         });
     }
+
+    _getSelectedIndex(type) {
+        if (this.queue.length === 0) return -1;
+
+        let selectedIndex = 0;
+
+        for (let i = 1; i < this.queue.length; i++) {
+            const current = this.queue[i];
+            const selected = this.queue[selectedIndex];
+
+            switch (type) {
+                case 'highest':
+                    if (current.priority > selected.priority) selectedIndex = i;
+                    break;
+                case 'lowest':
+                    if (current.priority < selected.priority) selectedIndex = i;
+                    break;
+                case 'oldest':
+                    if (current.order < selected.order) selectedIndex = i;
+                    break;
+                case 'newest':
+                    if (current.order > selected.order) selectedIndex = i;
+                    break;
+            }
+        }
+
+        return selectedIndex;
+    }
 }
